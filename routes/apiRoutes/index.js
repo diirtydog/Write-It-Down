bca61c3058339e362e54303eefb5ad324df17df8
diirtydog <andrew.derek.wales@gmail.com>
2022-01-21 14:36:40 -0600

    Finally figured out the correct pathways for the application to reload saved notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const {
     filterByQuery,
-    findByTitle,
+    findByid,
     createNewNote,
     validateNote
 } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
+const { id } = require('prelude-ls');
 
 router.get('/notes', (req, res) => {
     let results = notes;
@@ -15,24 +16,46 @@ router.get('/notes', (req, res) => {
     res.json(results);
 });
 
-router.get('/notes/:title', (req, res) => {
-    const results = findByTitle(req.params.title, notes);
-    if (results) {
-        res.json(results);
-    } else {
-        res.send(404);
-    }
-});
-
 router.post('/notes', (req, res) => {
-    req.body.title = notes.length.toString();
+    req.body.id = notes.length.toString();
     
-    if (!validateNotes(req.body)) {
+    if (!validateNote(req.body)) {
         res.status(400).send({ message: "This ain't the right damn format!"});
     } else {
         const note = createNewNote(req.body, notes);
         res.json(note);
     }
 });
+
+router
+    .route("/notes/:id")
+    .get((req, res) => {
+        const results = findByid(req.params.id, notes);
+        if (results) {
+            res.json(results);
+        } else {
+            res.send(404);
+        }
+    })
+    .delete((req, res) => {
+        console.info('delete')
+        const results = findByid(req.params.id, notes);
+        if (results) {
+            (results);
+        } else {
+            res.send(404);
+        }
+    })
+
+router.param("id", (req, res, next, id) => {
+    req.note = notes[id]
+    console.log(id);
+    next()
+})
+// router.get('/notes/:id', (req, res) => {
+    
+// });
+
+// router.delete it looks like the id one
 
 module.exports = router;
